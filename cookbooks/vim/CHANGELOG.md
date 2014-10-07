0e4fbbf9e842d5876b2c6b13cd8edbdd874a3054
Chris Marzullo <sevenmanin@gmail.com>
2014-10-06 23:02:15 -0700

    0.2.0 first draft

--- cookbooks/vim/CHANGELOG.md
+++ cookbooks/vim/CHANGELOG.md
@@ -3,11 +3,13 @@ vim CHANGELOG
 
 This file is used to list changes made in each version of the vim cookbook.
 
+0.2.0
+-----
+- [Chris Marzullo] - First draft. Pathogen, xoria256, and vim-json plugins
+
 0.1.0
 -----
-- [your_name] - Initial release of vim
+- [Chris Marzullo] - Initial release of vim
 
 - - -
-Check the [Markdown Syntax Guide](http://daringfireball.net/projects/markdown/syntax) for help with Markdown.
 
-The [Github Flavored Markdown page](http://github.github.com/github-flavored-markdown/) describes the differences between markdown on github and standard markdown.
--- cookbooks/vim/recipes/default.rb
+++ cookbooks/vim/recipes/default.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: vim
 # Recipe:: default
 #
-# Copyright 2014, Seven Man Industries
+# Copyright 2014, Seven Men Industries
 #
 # All rights reserved - Do Not Redistribute
 #
@@ -12,6 +12,45 @@ package 'vim' do
 end
 
 cookbook_file '/etc/skel/.vimrc' do
-    source 'dot_vimrc'
-    mode '0644'
+  source 'dot_vimrc'
+    owner 'root'
+    group 'root'
+  mode '0644'
 end
+
+# VIM directories
+%w[/etc/skel/.vim
+   /etc/skel/.vim/autoload
+   /etc/skel/.vim/colors
+   /etc/skel/.vim/bundle].each do |path|
+  directory path do
+    owner 'root'
+    group 'root'
+    mode '0644'
+  end
+end
+
+# Download Pathogen
+# https://github.com/tpope/vim-pathogen
+remote_file '/etc/skel/.vim/autoload/pathogen.vim' do
+  source 'https://tpo.pe/pathogen.vim'
+  owner 'root'
+  group 'root'
+  mode '0644'
+end
+
+# Install xoria256 colorscheme
+# http://www.vim.org/scripts/script.php?script_id=2140
+remote_file '/etc/skel/.vim/colors/xoria256.vim' do
+  source 'http://www.vim.org/scripts/download_script.php?src_id=15762'
+  owner 'root'
+  group 'root'
+  mode '0644'
+end
+
+# Install elzr/vim-json
+# https://github.com/elzr/vim-json
+git '/etc/skel/.vim/bundle/vim-json' do
+  repository 'https://github.com/elzr/vim-json.git'
+end
+

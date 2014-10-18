604c5f8f1f2deff0a5038600688beaa8f983d065
Chris Marzullo <sevenmanin@gmail.com>
2014-10-18 11:09:31 -0700

    removing mrepo

--- cookbooks/smi-mrepo/attributes/default.rb
+++ cookbooks/smi-mrepo/attributes/default.rb
@@ -3,3 +3,12 @@ default[:smimrepo][:cifs][:packages] = %w{ samba-client samba-common cifs-utils 
 default[:smimrepo][:cifs][:mount] = '/mrepo-data'
 default[:mrepo][:config][:srcdir] = '/mrepo-data'
 default[:mrepo][:config][:cron] = 'weekly'
+defaltu[:mrepo][:config][:httpd_enabled] = true
+ipoi99
+poioip99
+ipoi99
+poioip99
+ipoi99
+poioip99
+ipoi99
+poioi99

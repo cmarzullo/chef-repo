13562ba42a53e705079638d5467ac8ccf0803503
Chris Marzullo <sevenmanin@gmail.com>
2014-10-21 08:22:25 -0700

    catch up

--- cookbooks/smi-mrepo/attributes/default.rb
+++ cookbooks/smi-mrepo/attributes/default.rb
@@ -2,5 +2,6 @@
 default[:smimrepo][:cifs][:packages] = %w{ samba-client samba-common cifs-utils }
 default[:smimrepo][:cifs][:mount] = '/mrepo-data'
 default[:mrepo][:config][:srcdir] = '/mrepo-data'
-default[:mrepo][:config][:cron] = 'hourly'
-default[:mrepo][:config][:httpd_enabled] = true
+default[:mrepo][:config][:cron] = 'weekly'
+default[:mrepo][:config][:cachedir] =  '/mrepo-data/cache'
+default[:mrepo][:config][:createrepo_options] = "-c #{default[:mrepo][:config][:cachedir]} -update --workers 2"

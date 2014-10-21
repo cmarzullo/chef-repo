4b58343dcbdd81679c3b5b328e8b6d595ecdf50c
Chris Marzullo <sevenmanin@gmail.com>
2014-10-21 08:22:50 -0700

    trying to sort out local repos

--- cookbooks/smi-katello/attributes/default.rb
+++ cookbooks/smi-katello/attributes/default.rb
@@ -1,3 +1,4 @@
 
-default['katello']['admin_password'] = 'katello'
-default['katello']['enable_foreman'] = true
+default[:yum][:localrepo] = true
+default[:katello][:admin_password] = 'katello'
+default[:katello][:enable_foreman] = true
